8d7f4aaacddf21a2bb160b1207bc5a28f8fe59be
Islam Nashaat <75180825+islamnashaat03@users.noreply.github.com>
2024-11-25 11:02:19 +0200

    add last section

--- content/js/main.js
+++ content/js/main.js
@@ -1,3 +1,27 @@
 $document.ready(function () {
     new WOW().init();
+
+
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:10,
+        responsiveClass:true,
+        nav:true,
+        dots:false,
+        responsive:{
+            0:{
+                items:1,
+                
+            },
+            600:{
+                items:2,
+                
+            },
+            1000:{
+                items:3,
+                
+                
+            }
+        }
+    })
 });
