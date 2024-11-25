b51788e06f55f897b72d5ad665f98ecdc7e88ff1
Islam Nashaat <75180825+islamnashaat03@users.noreply.github.com>
2024-11-25 16:37:50 +0200

    end project

--- content/js/main.js
+++ content/js/main.js
@@ -1,27 +1,25 @@
-$document.ready(function () {
+$(document).ready(function () {
     new WOW().init();
 
-
+    // Initialize Owl Carousel
     $('.owl-carousel').owlCarousel({
-        loop:true,
-        margin:10,
-        responsiveClass:true,
-        nav:true,
-        dots:false,
-        responsive:{
-            0:{
-                items:1,
-                
-            },
-            600:{
-                items:2,
-                
-            },
-            1000:{
-                items:3,
-                
-                
-            }
+        loop: true,
+        margin: 10,
+        responsiveClass: true,
+        nav: true,
+        dots: false,
+        responsive: {
+            0: { items: 1 },
+            600: { items: 2 },
+            1000: { items: 3 }
         }
-    })
+    });
+
+    // Toggle mobile navbar
+    $('.nav-icon').click(function () {
+        $('.nav-mobile').toggleClass('active');
+    });
+    $('.close-icon').click(function () {
+        $('.nav-mobile').removeClass('active');
+    });
 });
